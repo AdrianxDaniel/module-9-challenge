8e9302828fda66d01b81f77c69fcceb5061717b1
Delta <adriandanieltapia21@gmail.com>
2024-04-18 00:00:57 -0700

    downloaded necesarry files , did the speedrun

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# module-9-challenge
-mod-9
+
+        # Mod9Challenge
+        
+        MIT
+
+        ### Description
+
+        readme file for module 9
+
+        ### Table of Contents
+        *Installation
+
+        ## Installation
+        To install run the following commands
+        ```
+        npm install inquirer
+        ```
+
+        ## Usage
+         javascript
+
+        MIT
+
+        ## Contributing
+        js
+
+        ## Test
+
+         npm run test
+
+        ## Questions
+
+        adriandanieltapia21@gmail.com
+        AdrianxDaniel
+
+    
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const path = require("path")
+const fs = require("fs");
+const inquirer = require("inquirer")
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is you email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is you project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a short description of your product?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What king of license should oyur project have?",
+    choices: ["MIT", "APACHE", "GPL", "None"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run a test?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log("Generating README....");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}))
+    })
+}
 
 // Function call to initialize app
 init();
